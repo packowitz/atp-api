82ee41bc3d1822c3ea9ed510aeaae1b3560aca4f
maxtuzz <maxtuzzolino@gmail.com>
2016-11-11 14:07:37 +1300

    Reintroduce entity scan

--- src/main/java/nz/pacworx/atp/Application.java
+++ src/main/java/nz/pacworx/atp/Application.java
@@ -2,8 +2,11 @@ package nz.pacworx.atp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EntityScan(basePackageClasses = {Application.class, Jsr310JpaConverters.class})
 @EnableScheduling
 @SpringBootApplication
 public class Application {
