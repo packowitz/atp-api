540f2aa94d99d09662acf0d9da4f561a593f94aa
maxtuzz <maxtuzzolino@gmail.com>
2016-11-17 18:24:25 +1300

    Refactored global error handling to include URL and message

--- src/main/java/io/pacworx/atp/controllers/advice/GlobalExceptionHandler.java
+++ src/main/java/io/pacworx/atp/controllers/advice/GlobalExceptionHandler.java
@@ -1,12 +1,11 @@
 package io.pacworx.atp.controllers.advice;
 
+import io.pacworx.atp.domain.ExceptionInfo;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Global exception handler
@@ -25,27 +24,47 @@ public class GlobalExceptionHandler {
     private final static String FORBIDDEN_REASON = "You don't have the required permission to do this action.";
     private final static String BAD_REQUEST_REASON = "Your request didn't have a very good format.";
 
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = GENERIC_ERROR)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = Exception.class)
-    public void handleBaseException(Exception e){
-        LOGGER.error(e != null ? e.getMessage() : GENERIC_ERROR);
+    @ResponseBody
+    ExceptionInfo
+    handleBaseException(HttpServletRequest req, Exception e){
+        String message = e != null ? e.getMessage() : GENERIC_ERROR;
+        LOGGER.error(message);
+
+        return new ExceptionInfo(req.getRequestURL().toString(), message);
     }
 
-    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = NOT_FOUND_REASON)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = NotFoundException.class)
-    public void handleNotFoundException(NotFoundException e){
-        LOGGER.info(e != null ? e.getMessage() : NOT_FOUND_REASON);
+    @ResponseBody ExceptionInfo
+    handleNotFoundException(HttpServletRequest req, NotFoundException e){
+        String message = e != null ? e.getMessage() : NOT_FOUND_REASON;
+
+        LOGGER.info(message);
+
+        return new ExceptionInfo(req.getRequestURL().toString(), message);
     }
 
-    @ResponseStatus(value = HttpStatus.FORBIDDEN, reason = FORBIDDEN_REASON)
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
     @ExceptionHandler(value = ForbiddenException.class)
-    public void handleForbiddenException(ForbiddenException e){
-        LOGGER.warn(e != null ? e.getMessage() : FORBIDDEN_REASON);
+    @ResponseBody ExceptionInfo
+    handleForbiddenException(HttpServletRequest req, ForbiddenException e){
+        String message = e != null ? e.getMessage() : FORBIDDEN_REASON;
+
+        LOGGER.info(message);
+
+        return new ExceptionInfo(req.getRequestURL().toString(), message);
     }
 
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = BAD_REQUEST_REASON)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = BadRequestException.class)
-    public void handleBadRequestException(BadRequestException e){
-        LOGGER.info(e != null ? e.getMessage() : BAD_REQUEST_REASON);
+    @ResponseBody ExceptionInfo
+    handleBadRequestException(HttpServletRequest req, BadRequestException e){
+        String message = e != null ? e.getMessage() : BAD_REQUEST_REASON;
+
+        LOGGER.info(message);
+
+        return new ExceptionInfo(req.getRequestURL().toString(), message);
     }
 }
---
+++ src/main/java/io/pacworx/atp/domain/ExceptionInfo.java
@@ -0,0 +1,16 @@
+package io.pacworx.atp.domain;
+
+/**
+ * Customized error messaging class
+ * Author: Max Tuzzolino
+ */
+
+public class ExceptionInfo {
+    public final String url;
+    public final String message;
+
+    public ExceptionInfo(String url, String message) {
+        this.url = url;
+        this.message = message;
+    }
+}
