8b573cdf3683dc97b7be1d25ef524ab38b12d3ae
maxtuzz <maxtuzzolino@gmail.com>
2017-01-02 16:08:22 +1300

    Rollback config changes for now. Added actuator to enhance our debugging capabilities

--- src/main/java/io/pacworx/atp/Application.java
+++ src/main/java/io/pacworx/atp/Application.java
@@ -3,13 +3,11 @@ package io.pacworx.atp;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackageClasses = {Application.class, Jsr310JpaConverters.class})
 @EnableScheduling
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
