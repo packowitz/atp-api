0948593eb94689a24c9f60c02279175baaacce09
maxtuzz <maxtuzzolino@gmail.com>
2017-01-05 15:11:11 +1300

    Register spring boot app as consul discovery client

--- src/main/java/io/pacworx/atp/Application.java
+++ src/main/java/io/pacworx/atp/Application.java
@@ -3,11 +3,13 @@ package io.pacworx.atp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackageClasses = {Application.class, Jsr310JpaConverters.class})
 @EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
