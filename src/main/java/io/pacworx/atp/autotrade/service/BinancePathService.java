b5d2271d4d5e8708e7bf62a9b50af184134b8b6d
packowitz <PhilippSchum@web.de>
2018-02-11 00:08:08 +1300

    error logging

--- src/main/java/io/pacworx/atp/autotrade/service/BinancePathService.java
+++ src/main/java/io/pacworx/atp/autotrade/service/BinancePathService.java
@@ -3,6 +3,7 @@ package io.pacworx.atp.autotrade.service;
 import io.pacworx.atp.autotrade.domain.*;
 import io.pacworx.atp.autotrade.domain.binance.BinanceOrderResult;
 import io.pacworx.atp.autotrade.domain.binance.BinanceTicker;
+import io.pacworx.atp.exception.BadRequestException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -126,8 +127,10 @@ public class BinancePathService {
                 } else {
                     log.info("Order " + orderResult.getOrderId() + " from path " + orderToCheck.getSubplanId() + " is in status: " + orderResult.getStatus());
                 }
-            } catch (Exception e) {
+            } catch (BadRequestException e) {
                 log.info("Order " + orderToCheck.getOrderId() + " from path " + orderToCheck.getSubplanId() + " failed to check status");
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
             }
         }
     }
